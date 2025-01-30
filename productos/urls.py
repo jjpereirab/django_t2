9e7dd712cb6eb69ea6e8c6d50d2e8506d25c5b49
jonnatan <jjpereirab@unal.edu.co>
2025-01-29 23:50:22 -0500

    clase 19, tarea_a: template, view y url para listar productos, con cbv y fbv

--- productos/urls.py
+++ productos/urls.py
@@ -3,6 +3,11 @@ from .views import vista_producto, VistaFormularioProducto, VistaListaProductos,
 
 
 urlpatterns = [ # todas estas urls llevan /productos/ al inicio
+    # clase 18, tarea
     path('<int:id>', vista_producto),
-    path('agregar', VistaFormularioProducto.as_view(), name='url_name_agregar_producto'), # clase 19, Formularios    
+    # clase 19, Formularios
+    path('agregar', VistaFormularioProducto.as_view(), name='url_name_agregar_producto'), 
+    # clase 19, tarea
+    path('lista_cbv', VistaListaProductos.as_view(), name='url_name_lista_productos_cbv'), 
+    path('lista_fbv', lista_productos, name='url_name_lista_productos_fbv'), 
 ]
--- productos/views.py
+++ productos/views.py
@@ -15,9 +15,21 @@ def vista_producto(request, *args, **kwargs):
 class VistaFormularioProducto(generic.FormView):
     template_name = "productos/agregar_producto.html"   # <--- SE TIENE QUE LLAMAR template_name
     form_class = FormularioProducto                     # <--- SE TIENE QUE LLAMAR form_class
-    success_url = reverse_lazy('url_name_agregar_producto') # <--- SE TIENE QUE LLAMAR success_url
+    # success_url = reverse_lazy('url_name_agregar_producto') # <--- SE TIENE QUE LLAMAR success_url
+    success_url = reverse_lazy('url_name_lista_productos_cbv') # <--- SE TIENE QUE LLAMAR success_url, **tarea**
 
     def form_valid(self, form):
         form.guardar()
         return super().form_valid(form)
 
+# clase 19, tarea
+from django.views.generic import ListView
+class VistaListaProductos(ListView):
+    model = Producto  # Specify the model
+    template_name = 'productos/lista_productos.html'  # Specify the template
+    context_object_name = 'items'  # Name for the context variable
+
+from django.shortcuts import render
+def lista_productos(request):
+    items = Producto.objects.all()  # Fetch all entries from the Item model
+    return render(request, 'productos/lista_productos.html', {'items': items})
